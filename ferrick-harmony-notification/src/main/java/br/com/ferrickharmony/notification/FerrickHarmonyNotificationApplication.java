39641f602ca709979fa7b1e2503266ea63e8ef94
Ranon Campos <ranonemerickcampos@gmail.com>
2026-08-24 21:34:50 -0300

    feat(microservices): add notification service and dockerize application
    - Initialize 'ferrick-harmony-notification' microservice for async messaging
    - Create optimized multi-stage Dockerfiles for both API and notification services
    - Update docker-compose.yml to include RabbitMQ, isolated databases, and app containers
    - Configure application environments for Docker network resolution

---
+++ ferrick-harmony-notification/src/main/java/br/com/ferrickharmony/notification/FerrickHarmonyNotificationApplication.java
@@ -0,0 +1,13 @@
+package br.com.ferrickharmony.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerrickHarmonyNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerrickHarmonyNotificationApplication.class, args);
+	}
+
+}
---
+++ ferrick-harmony-notification/src/test/java/br/com/ferrickharmony/notification/FerrickHarmonyNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ferrickharmony.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerrickHarmonyNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
